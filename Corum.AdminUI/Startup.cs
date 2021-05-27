305ed4431bc91847f89048772c22c533e6eba585
SergioKredo <sergeykredenser@gmail.com>
2021-05-27 17:10:52 +0300

    Add HangFire task

--- Corum.AdminUI/Startup.cs
+++ Corum.AdminUI/Startup.cs
@@ -1,5 +1,16 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using CorumAdminUI;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security.Cookies;
+using CorumAdminUI.Models;
+using Hangfire;
+using Hangfire.Dashboard;
+using Hangfire.SqlServer;
+using Hangfire.AspNet;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 [assembly: OwinStartupAttribute(typeof(BarnivannAdminUI.Startup))]
 namespace BarnivannAdminUI
@@ -9,8 +20,11 @@ namespace BarnivannAdminUI
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
-        }
+            app.UseHangfireAspNet(GetHangfireServers);
+            app.UseHangfireDashboard();
 
-       
+            // Let's also create a sample background job
+            BackgroundJob.Enqueue(() => Debug.WriteLine("Hello world from Hangfire!"));
+        }
     }
 }

a2fd31c171b577eb879f0b6b86572981338eed9d
SergioKredo <sergeykredenser@gmail.com>
2021-05-27 17:11:50 +0300

    Add models hangFire tasks

---
+++ Corum.AdminUI/HangFireTasks/HangFireTasks.cs
@@ -0,0 +1,36 @@
+﻿using Corum.Models;
+using Corum.Models.Tender;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+
+namespace CorumAdminUI.HangFireTasks
+{
+    public class HangFireTasks
+    {
+        protected Corum.Models.ICorumDataProvider context;
+        List<RegisterTenders> listRegistryTenders;
+
+        public HangFireTasks()
+        {
+            listRegistryTenders = new List<RegisterTenders>();
+            context = DependencyResolver.Current.GetService<ICorumDataProvider>();
+        }
+        public async Task GetStatusTender()
+        {
+            try
+            {
+                listRegistryTenders = context.GetRegisterTenders();
+            }
+            catch (Exception e)
+            { 
+            
+            }
+            await Task.Run(() => Debug.WriteLine($"{listRegistryTenders[0].processValue}"));
+        }
+    }
+}
